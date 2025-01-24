a726e3f97a9e99ba823ce60970b56dd6e7c300ec
Marcus Lim <marcus@bukku.my>
2025-01-24 10:39:29 +0800

    add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,49 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <button className="bg-blue-500 hover:bg-blue-700 hover:cursor-pointer text-white h-10 font-bold py-2 px-4 rounded">
+          Tailwind button
+        </button>
+
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th className="px-6 py-3">Name</th>
+              <th className="px-6 py-3">Age</th>
+              <th className="px-6 py-3">Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+              <td className="px-6 py-4">Mike</td>
+              <td className="px-6 py-4">32</td>
+              <td className="px-6 py-4">10 Downing Street</td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+              <td>John</td>
+              <td>42</td>
+              <td>10 Downing Street</td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+              <td>Lola</td>
+              <td>11</td>
+              <td>11 Downing Street</td>
+            </tr>
+            <tr>
+              <td>Kate</td>
+              <td>20</td>
+              <td>12 Downing Street</td>
+            </tr>
+          </tbody>
+        </table>
+
+
+
+        <div  className="bg-blue-100 h-80">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+
+
         <Image
           className={styles.logo}
           src="/next.svg"
