99554509e6df8c250e0750b2600b58ae62fbaa40
Marcus Lim <marcus@bukku.my>
2025-01-24 10:11:54 +0800

    add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,65 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Button, Table } from "antd";
 
 export default function Home() {
+  const dataSource = [
+    {
+      key: '1',
+      name: 'Mike',
+      age: 32,
+      address: '10 Downing Street',
+    },
+    {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+    },
+    {
+      key: '3',
+      name: 'Lola',
+      age: 11,
+      address: '11 Downing Street',
+    },
+    {
+      key: '4',
+      name: 'Kate',
+      age: 20,
+      address: '12 Downing Street',
+    },
+  ];
+  
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
+  
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <Button type="primary">ANTD Button</Button>
+
+        <Table dataSource={dataSource} columns={columns} />
+
+        <div style={{background: "aliceblue", height: "200px"}}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        
         <Image
           className={styles.logo}
           src="/next.svg"
